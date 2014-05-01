841cde45b039cdc8b25f9cc44bd49ba848cbb18a
Wolfgang Barth <wob@swobspace.net>
2014-05-01 16:40:25 +0200

    some translation checks for namespaces

---
+++ test/helpers/wobauth/translation_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+module Wobauth
+  class TranslationTest < ActionController::TestCase
+
+    should "translate Role for model and controller actions" do
+      assert_match "Rolle", I18n.t('activerecord.models.' + 
+                              Wobauth::Role.model_name.i18n_key.to_s) 
+      assert_match "Rollen", I18n.t('controller.wobauth/roles')
+      assert_match "Name", I18n.t('activerecord.attributes.name')
+      # assert_match "Rolle erstellen", t('w^.attributes.name')
+    end
+
+  end
+end
