fcc2585da067b1015c7dcc6536769c6ebcc64169
Wolfgang Barth <wob@swobspace.net>
2014-05-01 12:28:55 +0200

    add generator stuff to test/dummy

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -20,6 +20,14 @@ module Dummy
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :de
     config.time_zone = 'Berlin'
+
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+    end
   end
 end
 
