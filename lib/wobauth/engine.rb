dc2a39b741c58dc56777604528496ef3e3f38fa6
Wolfgang Barth <wob@swobspace.net>
2014-05-01 13:56:15 +0200

    add local scaffold path for templates

--- lib/wobauth/engine.rb
+++ lib/wobauth/engine.rb
@@ -1,5 +1,9 @@
 module Wobauth
   class Engine < ::Rails::Engine
     isolate_namespace Wobauth
+
+    config.generators do |g|
+      g.templates.unshift File::expand_path('../../templates', __FILE__)
+    end
   end
 end
