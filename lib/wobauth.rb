57eda134a80440a250a1bb6cc56a26e87cadca18
Wolfgang Barth <wob@swobspace.net>
2014-05-04 16:54:53 +0200

    make polymorphic types for authority configurable

--- lib/wobauth.rb
+++ lib/wobauth.rb
@@ -16,8 +16,21 @@ module Wobauth
     @@user_class.constantize
   end
 
+  # returns the user_class as underscored symbol for source: statements
   def self.user_source
     @@user_class.underscore.to_sym
   end
+
+  # authorizable types
+  #
+  mattr_reader :authorizable_types 
+  @@authorizable_types = [ "User", "Group" ]
+  
+  # authorized_for types
+  # Objects on which Wobauth should set authorization
+  #
+  mattr_accessor :authorized_for_types 
+  @@authorized_for_types = [ ]
+  
 
 end
