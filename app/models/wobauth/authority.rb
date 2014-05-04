d714db8b851849de89db4aaede02be4f0e38d35c
Wolfgang Barth <wob@swobspace.net>
2014-05-04 17:08:52 +0200

    complete model authority

--- app/models/wobauth/authority.rb
+++ app/models/wobauth/authority.rb
@@ -1,7 +1,13 @@
 module Wobauth
   class Authority < ActiveRecord::Base
-    belongs_to :authorizable
+    # -- associations
+    belongs_to :authorizable,   polymorphic: true
     belongs_to :role
-    belongs_to :authorized_for
+    belongs_to :authorized_for, polymorphic: true
+
+    # -- configuration
+    # -- validations and callbacks
+    validates :authorizable_id, :authorizable_type,
+              :role_id, presence: true
   end
 end
--- test/models/wobauth/authority_test.rb
+++ test/models/wobauth/authority_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 module Wobauth
   class AuthorityTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    should belong_to(:authorizable)
+    should belong_to(:role)
+    should belong_to(:authorized_for)
+
+    should validate_presence_of(:authorizable_id)
+    should validate_presence_of(:authorizable_type)
+    should validate_presence_of(:role_id)
   end
 end
