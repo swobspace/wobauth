6c7a633530b6bc68e11667e2e2adaef364f7df83
Wolfgang Barth <wob@swobspace.net>
2014-05-04 11:52:30 +0200

    add Wobauth.setup and accessor for user_class

--- lib/wobauth.rb
+++ lib/wobauth.rb
@@ -1,4 +1,19 @@
 require "wobauth/engine"
 
 module Wobauth
+
+  def self.setup
+    yield self
+  end
+
+  # class_name for the user model of your application.
+  # Default: "User"
+  #
+  mattr_accessor :user_class
+  @@user_class = "User"
+
+  def self.user_class
+    @@user_class.constantize
+  end
+
 end
