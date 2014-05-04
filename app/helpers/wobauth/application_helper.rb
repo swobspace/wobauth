32ed4c16525e56dc600d9596b7045093ef6883e8
Wolfgang Barth <wob@swobspace.net>
2014-05-04 19:08:51 +0200

    polymorphic selector helper for authorities

--- app/helpers/wobauth/application_helper.rb
+++ app/helpers/wobauth/application_helper.rb
@@ -1,5 +1,17 @@
 module Wobauth
   module ApplicationHelper
     include Wobapphelpers::Helpers::All
+
+    def polymorphic_selector(form, poly, types)
+      if types.any?
+	msg = ""
+	msg += form.input("#{poly}_type".to_sym, collection: types)
+	types.each do |type|
+	  msg += form.input("#{poly}_id".to_sym, collection: type.constantize.all,
+		   label: t("activerecord.models." + type.underscore))
+	end
+	msg.html_safe
+      end
+    end
   end
 end
