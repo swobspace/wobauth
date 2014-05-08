db45a210fd32673f11b82a67498c20e4b0ae0d75
Wolfgang Barth <wob@swobspace.net>
2014-05-08 16:10:50 +0200

    rebuild namespace, don't work otherwise

--- lib/concerns/models/user.rb
+++ lib/concerns/models/user.rb
@@ -1,18 +1,20 @@
-module Wobauth::Concerns::Models::User
-  extend ActiveSupport::Concern
+module Wobauth
+  module Concerns
+    module Models::User
+      extend ActiveSupport::Concern
 
-  included do
-    has_many :authorities, as: :authorizable, dependent: :destroy
-    has_many :roles,       through: :authorities
-    has_many :memberships, dependent: :destroy
-    has_many :group_roles, through: :groups, source: :roles
-    has_many :groups, -> { uniq }, through: :memberships
-  end
+      included do
+	has_many :authorities, as: :authorizable, dependent: :destroy
+	has_many :roles,       through: :authorities
+	has_many :memberships, dependent: :destroy
+	has_many :group_roles, through: :groups, source: :roles
+	has_many :groups, -> { uniq }, through: :memberships
+      end
 
-  def to_s
-    email
-  end
+      def to_s
+	email
+      end
 
-  module ClassMethods
+    end
   end
 end
