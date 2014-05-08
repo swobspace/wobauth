43de190e987ca8b09271254856e3b89fd3c6c8ab
Wolfgang Barth <wob@swobspace.net>
2014-05-08 13:57:33 +0200

    get translation test running

--- test/helpers/wobauth/translation_test.rb
+++ test/helpers/wobauth/translation_test.rb
@@ -7,7 +7,7 @@ module Wobauth
       assert_match "Rolle", I18n.t('activerecord.models.' + 
                               Wobauth::Role.model_name.i18n_key.to_s) 
       assert_match "Rollen", I18n.t('controller.wobauth/roles')
-      assert_match "Name", I18n.t('activerecord.attributes.name')
+      assert_match "Name", I18n.t('attributes.name')
       # assert_match "Rolle erstellen", t('w^.attributes.name')
     end
 
