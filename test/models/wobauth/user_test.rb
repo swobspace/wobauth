9addd92b78a5267a5d795c6fb3903c8ed2f42358
Wolfgang Barth <wob@swobspace.net>
2014-05-10 14:13:50 +0200

    user model test

--- test/models/wobauth/user_test.rb
+++ test/models/wobauth/user_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 module Wobauth
   class UserTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    should have_many(:authorities)
+    should have_many(:roles).through(:authorities)
+    should have_many(:memberships)
+    should have_many(:group_roles).through(:groups).source(:roles)
+    should have_many(:groups).through(:memberships)
+
+    should validate_presence_of(:username)
+    should validate_presence_of(:email)
+    should validate_uniqueness_of(:username)
+
   end
 end
