e7519066056c63895826a1670e69d81f4bdb2798
Wolfgang Barth <wob@swobspace.net>
2014-05-10 09:54:01 +0200

    cleanup test database

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -13,31 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140508120810) do
 
-  create_table "users", force: true do |t|
-    t.text     "gruppen"
-    t.string   "sn"
-    t.string   "givenname"
-    t.string   "displayname"
-    t.string   "telephone"
-    t.string   "userprincipalname"
-    t.string   "username",               default: "", null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   create_table "wobauth_authorities", force: true do |t|
     t.integer  "authorizable_id"
     t.string   "authorizable_type"
