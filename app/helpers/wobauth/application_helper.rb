81d045466942b2e5d711c93ac492a120bf9becb1
Wolfgang Barth <wob@swobspace.net>
2014-05-11 11:56:08 +0200

    improve polymorphic_selector

--- app/helpers/wobauth/application_helper.rb
+++ app/helpers/wobauth/application_helper.rb
@@ -6,10 +6,15 @@ module Wobauth
       if types.any?
 	msg = ""
 	msg += form.input("#{poly}_type".to_sym, collection: types)
-	types.each do |type|
-	  msg += form.input("#{poly}_id".to_sym, collection: type.constantize.all,
-		   label: t("activerecord.models." + type.underscore))
-	end
+	# types.each do |type|
+	#   msg += form.input("#{poly}_id".to_sym, collection: type.constantize.all,
+  	# 	   label: t("activerecord.models." + type.underscore))
+	# end
+        collections = types.map {|t| t.constantize}
+        msg += form.input(
+                 "#{poly}_id".to_sym, collection: collections,
+                 as: :grouped_select, group_method: :all
+               )
 	msg.html_safe
       end
     end
