0cc3fef067ea76f59fe319fafa85bdcb8c40a440
Wolfgang Barth <wob@swobspace.net>
2014-05-11 16:23:52 +0200

    adapt template to current possibilities

--- lib/generators/templates/initializers/wobauth.rb
+++ lib/generators/templates/initializers/wobauth.rb
@@ -1,8 +1,15 @@
 Wobauth.setup do |config|
   #
-  # User class used in your application
-  # Must be set as string!
-  # Default: "Wobauth::User"
-  # config.user_class = "Wobauth::User"
+  # Configuration for Authorization
+  # 1. Subject: Authorizable
+  # do not change it unless you know exactly what you are doing
+  #
+  # config.authorizable_types = [ "Wobauth::User", "Wobauth::Group" ]
+  #
+  # 2. Object: Authorized_for
+  # depends on your application ...
+  # default: []
+  #
+  # config.authorized_for_types = [ "MyClass", ...]
   #
 end
