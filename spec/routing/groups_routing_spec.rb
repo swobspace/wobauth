0652b7d6be7b2a96714855a2a2b0c223c726332d
Wolfgang Barth <wob@swobspace.net>
2017-12-22 14:06:10 +0100

    fix spec; no use of modules in routing yet

--- spec/routing/groups_routing_spec.rb
+++ spec/routing/groups_routing_spec.rb
@@ -39,36 +39,36 @@ RSpec.describe Wobauth::GroupsController, type: :routing do
   end
 
   describe "routing authorities" do
-    it "routes to wobauth/groups/authorities#index" do
-      expect(:get => "/groups/9/authorities").to route_to(controller: 'wobauth/groups/authorities', action: "index", group_id: "9")
+    it "routes to wobauth/authorities#index" do
+      expect(:get => "/groups/9/authorities").to route_to(controller: 'wobauth/authorities', action: "index", group_id: "9")
     end
 
     it "routes to #new" do
-      expect(:get => "/groups/9/authorities/new").to route_to(controller: 'wobauth/groups/authorities', action: "new", group_id: "9")
+      expect(:get => "/groups/9/authorities/new").to route_to(controller: 'wobauth/authorities', action: "new", group_id: "9")
     end
 
     it "routes to #show" do
-      expect(:get => "/groups/9/authorities/1").to route_to(controller: 'wobauth/groups/authorities', action: "show", :id => "1", group_id: "9")
+      expect(:get => "/groups/9/authorities/1").to route_to(controller: 'wobauth/authorities', action: "show", :id => "1", group_id: "9")
     end
 
     it "routes to #edit" do
-      expect(:get => "/groups/9/authorities/1/edit").to route_to(controller: 'wobauth/groups/authorities', action: "edit", :id => "1", group_id: "9")
+      expect(:get => "/groups/9/authorities/1/edit").to route_to(controller: 'wobauth/authorities', action: "edit", :id => "1", group_id: "9")
     end
 
     it "routes to #create" do
-      expect(:post => "/groups/9/authorities").to route_to(controller: 'wobauth/groups/authorities', action: "create", group_id: "9")
+      expect(:post => "/groups/9/authorities").to route_to(controller: 'wobauth/authorities', action: "create", group_id: "9")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/groups/9/authorities/1").to route_to(controller: 'wobauth/groups/authorities', action: "update", :id => "1", group_id: "9")
+      expect(:put => "/groups/9/authorities/1").to route_to(controller: 'wobauth/authorities', action: "update", :id => "1", group_id: "9")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/groups/9/authorities/1").to route_to(controller: 'wobauth/groups/authorities', action: "update", :id => "1", group_id: "9")
+      expect(:patch => "/groups/9/authorities/1").to route_to(controller: 'wobauth/authorities', action: "update", :id => "1", group_id: "9")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/groups/9/authorities/1").to route_to(controller: 'wobauth/groups/authorities', action: "destroy", :id => "1", group_id: "9")
+      expect(:delete => "/groups/9/authorities/1").to route_to(controller: 'wobauth/authorities', action: "destroy", :id => "1", group_id: "9")
     end
 
   end
