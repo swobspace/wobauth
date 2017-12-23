aa3374c36502250f16395081bd9af23971bec392
Wolfgang Barth <wob@swobspace.net>
2017-12-23 11:52:19 +0100

    rewrite, but url_helper doesn't work

--- spec/views/wobauth/roles/index.html.erb_spec.rb
+++ spec/views/wobauth/roles/index.html.erb_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "roles/index", type: :view do
+RSpec.describe "wobauth/roles/index", type: :view do
   before(:each) do
     assign(:roles, [
-      Role.create!(
-        :name => "Name"
-      ),
-      Role.create!(
-        :name => "Name"
-      )
+      FactoryBot.create(:role, name: "XYZ"),
+      FactoryBot.create(:role, name: "ABC"),
     ])
   end
 
   it "renders a list of roles" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "XYZ".to_s, :count => 1
+    assert_select "tr>td", :text => "ABC".to_s, :count => 1
   end
 end
