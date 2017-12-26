50d4d61dea7a97454891c356997c983380f37a8c
Wolfgang Barth <wob@swobspace.net>
2017-12-26 13:59:55 +0100

    remove doubles

--- spec/controllers/wobauth/authority_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Wobauth::AuthoritiesController, type: :controller do
-  routes { Wobauth::Engine.routes }
-
-  fixtures :users, :roles
-  set_fixture_class users: Wobauth::User, roles: Wobauth::Role
-
-end
