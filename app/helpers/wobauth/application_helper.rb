78a506c7bd80d6f24101137ba3f2d7974e1096e8
Wolfgang Barth <wob@swobspace.net>
2018-07-01 10:21:24 +0200

    starting redesign polymorphic helper for branch 4.0

--- app/helpers/wobauth/application_helper.rb
+++ app/helpers/wobauth/application_helper.rb
@@ -6,9 +6,14 @@ module Wobauth
       if types.any?
         # -- type selection
 	msg = ""
-	msg += form.input("#{poly}_type".to_sym, collection: types,
-                 label_method: lambda {|x| t('activerecord.models.' + x.underscore)}
-               )
+	msg += form.input "#{poly}_type".to_sym, 
+                 collection: types.map { |t| 
+                   [
+                     t('activerecord.models.' + t.underscore),
+                     t,
+                     { data: { url: data_url(t) } }
+                   ]
+                 }
         # -- object id selection
         collections = types.map {|t| t.constantize}
         msg += form.input(
@@ -30,6 +35,17 @@ module Wobauth
     def admin_active_class
       if [:users, :groups, :memberships, :roles, :authorities].include?(controller.controller_name.to_sym)
         "active"
+      end
+    end
+
+    def data_url(t)
+      if String(t).constantize.respond_to?(:data_url)
+        String(t).constantize.data_url
+      else
+	raise RuntimeError, <<-EOF
+  Please implement #{t}.data_url (new since wobauth 4.0)
+  See https://github.com/swobspace/wobauth for more information
+  EOF
       end
     end
   end
---
+++ spec/helpers/wobauth/application_helper_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+require "cancan/matchers"
+
+RSpec.describe Wobauth::ApplicationHelper, :type => :helper do
+  include Devise::Test::ControllerHelpers
+
+  describe "#data_url" do
+    context "with nonexisting class method data_url" do
+      before(:each) do
+        expect(Wobauth::User).to receive(:respond_to?).with(:data_url).and_return(false)
+      end
+      it "raise a RuntimeError" do
+        expect { 
+          helper.data_url("Wobauth::User") 
+        }.to raise_error(RuntimeError)
+      end
+
+    end
+    context "with existing class method data_url" do
+      let(:url) { "/wobauth/anywhere/anypath" }
+      before(:each) do
+        expect(Wobauth::User).to receive(:respond_to?).with(:data_url).and_return(true)
+        expect(Wobauth::User).to receive(:data_url).with("Wobauth::User").and_return(url)
+      end
+
+      it { expect(helper.data_url("Wobauth::User")).to eq(url) }
+    end
+  end
+
+end
