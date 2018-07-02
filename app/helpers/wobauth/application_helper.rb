f573aff5bda45a9c1614279f4ac8454fdd46613d
Wolfgang Barth <wob@swobspace.net>
2018-07-02 18:18:01 +0200

    rewrite polymorphic helper
      don't allow change _type if it is always set, i.e.
      - from routing (users/x/authorities/new)
      - in #edit

--- app/helpers/wobauth/application_helper.rb
+++ app/helpers/wobauth/application_helper.rb
@@ -2,21 +2,17 @@ module Wobauth
   module ApplicationHelper
     include Wobapphelpers::Helpers::All
 
-    def polymorphic_selector(form, poly, types)
-      if types.any?
-        # -- type selection
-	msg = ""
-	msg += form.input("#{poly}_type".to_sym, collection: types, 
-                 label_method: lambda {|x| t('activerecord.models.' + x.underscore)}
-               )
-        # -- object id selection
-        collections = types.map {|t| t.constantize}
-        msg += form.input(
-                 "#{poly}_id".to_sym, collection: collections,
-                 as: :grouped_select, group_method: :all
-               )
-	msg.html_safe
+    def polymorphic_selector(form, poly, types, group_method = :all)
+      msg = ""
+      # -- object available?
+      if form.object.send(poly).present?
+	msg += poly_type_display(form, poly)
+	msg += poly_id_select(form, poly, form.object.send("#{poly}_type"), :all)
+      else
+	msg += poly_type_select(form, poly, types)
+	msg += poly_id_select(form, poly, types, group_method)
       end
+      msg.html_safe
     end
 
     def navigation_admin_links
@@ -32,5 +28,26 @@ module Wobauth
         "active"
       end
     end
+
+  private
+
+    def poly_type_select(f, poly, types)
+      f.input "#{poly}_type".to_sym, collection: types,
+              label_method: lambda {|x| t('activerecord.models.' + x.underscore)}
+    end
+
+    def poly_type_display(f, poly)
+     f.input("#{poly}_type".to_sym, collection: Array(f.object.send("#{poly}_type")),
+       label_method: lambda {|x| t('activerecord.models.' + x.underscore)},
+       disabled: true) +
+     f.hidden_field("#{poly}_type".to_sym, value: f.object.send("#{poly}_type"))
+    end
+
+    def poly_id_select(f, poly, types, group_method)
+      collections = Array(types).map {|t| t.constantize}
+      f.input "#{poly}_id".to_sym, collection: collections,
+                 as: :grouped_select, group_method: group_method
+    end
+
   end
 end
