86bd8222f1ebd410724e90fc1d69e316c7a0c523
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2020-01-03 12:16:43 +0100

    rails app:update; adapt initializers, environments and application.rb

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -2,90 +2,90 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231084355) do
+ActiveRecord::Schema.define(version: 2020_01_03_102334) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "subject", limit: 255
+    t.string "subject"
     t.text "message"
     t.integer "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
   create_table "wobauth_authorities", force: :cascade do |t|
     t.integer "authorizable_id"
-    t.string "authorizable_type", limit: 255
+    t.string "authorizable_type"
     t.integer "role_id"
     t.integer "authorized_for_id"
-    t.string "authorized_for_type", limit: 255
+    t.string "authorized_for_type"
     t.date "valid_from"
     t.date "valid_until"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["authorizable_id"], name: "index_wobauth_authorities_on_authorizable_id"
     t.index ["authorized_for_id"], name: "index_wobauth_authorities_on_authorized_for_id"
     t.index ["role_id"], name: "index_wobauth_authorities_on_role_id"
   end
 
   create_table "wobauth_groups", force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "description", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "wobauth_memberships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "group_id"
     t.boolean "auto", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_wobauth_memberships_on_group_id"
     t.index ["user_id"], name: "index_wobauth_memberships_on_user_id"
   end
 
   create_table "wobauth_roles", force: :cascade do |t|
-    t.string "name", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "wobauth_users", force: :cascade do |t|
-    t.string "username", limit: 255, default: "", null: false
+    t.string "username", default: "", null: false
     t.text "gruppen"
-    t.string "sn", limit: 255
-    t.string "givenname", limit: 255
-    t.string "displayname", limit: 255
-    t.string "telephone", limit: 255
-    t.string "active_directory_guid", limit: 255
-    t.string "userprincipalname", limit: 255
-    t.string "email", limit: 255, default: "", null: false
-    t.string "encrypted_password", limit: 255, default: "", null: false
-    t.string "reset_password_token", limit: 255
+    t.string "sn"
+    t.string "givenname"
+    t.string "displayname"
+    t.string "telephone"
+    t.string "active_directory_guid"
+    t.string "userprincipalname"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip", limit: 255
-    t.string "last_sign_in_ip", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "title", default: ""
     t.string "position", default: ""
     t.string "department", default: ""
