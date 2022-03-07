3d912abf2ab52f090dabe1f906b1c35cc33fc115
Wolfgang Barth (wob42) <wob@swobspace.net>
2022-03-07 13:48:49 +0100

    fix source_type

--- app/models/wobauth/role.rb
+++ app/models/wobauth/role.rb
@@ -4,10 +4,10 @@ module Wobauth
     has_many :authorities
     has_many :users,  through:     :authorities,
                       source:      :authorizable,
-                      source_type: Wobauth::User
+                      source_type: 'Wobauth::User'
     has_many :groups, through:     :authorities,
                       source:      :authorizable,
-                      source_type: Wobauth::Group
+                      source_type: 'Wobauth::Group'
     # -- configuration
     # -- validations and callbacks
     validates :name, presence: true, uniqueness: {case_sensitive: true}
