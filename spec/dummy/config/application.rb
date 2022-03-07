11cceef23620830122b84dcb71ea43750996a874
Wolfgang Barth (wob42) <wob@swobspace.net>
2022-03-07 16:43:27 +0100

    takeover application.rb from a rails 7 app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,10 +1,21 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:all)
+Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
