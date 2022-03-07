f7662f5ad4dd1336198e600822d23eb667f18e0e
Wolfgang Barth (wob42) <wob@swobspace.net>
2022-03-07 13:33:35 +0100

    run wobauth generators

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2020_01_03_102334) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_07_123227) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: nil, null: false
@@ -24,6 +24,73 @@ ActiveRecord::Schema[7.0].define(version: 2020_01_03_102334) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.index ["category_id"], name: "index_posts_on_category_id"
+  end
+
+  create_table "wobauth_authorities", force: :cascade do |t|
+    t.integer "authorizable_id"
+    t.string "authorizable_type"
+    t.integer "role_id"
+    t.integer "authorized_for_id"
+    t.string "authorized_for_type"
+    t.date "valid_from"
+    t.date "valid_until"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["authorizable_id"], name: "index_wobauth_authorities_on_authorizable_id"
+    t.index ["authorized_for_id"], name: "index_wobauth_authorities_on_authorized_for_id"
+    t.index ["role_id"], name: "index_wobauth_authorities_on_role_id"
+  end
+
+  create_table "wobauth_groups", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "wobauth_memberships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.boolean "auto", default: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["group_id"], name: "index_wobauth_memberships_on_group_id"
+    t.index ["user_id"], name: "index_wobauth_memberships_on_user_id"
+  end
+
+  create_table "wobauth_roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "wobauth_users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.text "gruppen"
+    t.string "sn"
+    t.string "givenname"
+    t.string "displayname"
+    t.string "telephone"
+    t.string "active_directory_guid"
+    t.string "userprincipalname"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.string "title", default: ""
+    t.string "position", default: ""
+    t.string "department", default: ""
+    t.string "company", default: ""
+    t.index ["reset_password_token"], name: "index_wobauth_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_wobauth_users_on_username", unique: true
   end
 
 end
