484da39f8b83894a7d0124a688f2c251d82afdff
Wolfgang Barth (wob42) <wob@swobspace.net>
2022-03-11 13:00:51 +0100

    starting create authority spec

---
+++ spec/features/create_authority_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+module Wobauth
+  RSpec.describe "Create Authority", type: :feature, js: true do
+    fixtures :users, :roles
+    set_fixture_class users: Wobauth::User, roles: Wobauth::Role
+
+    let(:cat) { Category.create(name: 'lastCategory') }
+
+    describe "visit authority_path" do
+      before(:each) do
+        login_admin
+        visit wobauth.new_authority_path
+      end
+
+      it "create a new authority" do
+        expect(page).to have_content("Berechtigung erstellen")
+        expect(page).to have_css("#authority_authorizable_type")
+        expect(page).to have_css("#authority_authorizable_id", visible: false)
+        expect(page).to have_css("#authority_authorized_for_type")
+        expect(page).to have_css("#authority_authorized_for_id", visible: false)
+        within "form#new_authority" do
+          select "Benutzer", from: "Authorisierbar/Typ"
+          sleep 0.5
+          select "one", from: "authority_authorizable_id"
+          select "RoleOne", from: "Rolle"
+          select "Kategorie", from: "Berechtigung auf/Typ"
+          sleep 0.5
+          save_and_open_screenshot()
+          select "lastCategory", from: "authority_authorized_for_id"
+        end
+        # save_and_open_screenshot
+        click_button("Berechtigung erstellen")
+        sleep 0.5
+        expect(page).to have_content("Berechtigung erfolgreich erstellt")
+        expect(page).to have_content("one")
+        expect(page).to have_content("RoleOne")
+        expect(page).to have_content("lastCategory")
+        save_and_open_screenshot()
+      end
+    end
+  end
+end
