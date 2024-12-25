614828b53038d74e05e8e2e6988a652636c836f0
Wolfgang Barth <wob@swobspace.net>
2024-12-25 16:12:17 +0100

    avoid breadcrumb_index for devise controllers

--- app/controllers/wobauth/application_controller.rb
+++ app/controllers/wobauth/application_controller.rb
@@ -10,7 +10,8 @@ module Wobauth
 
     # -- breadcrumbs
     include Wobapphelpers::Breadcrumbs
-    before_action :add_breadcrumb_index, only: [:index]
+    before_action :add_breadcrumb_index,
+                  :if => proc {|c| !devise_controller? && c.action_name == 'index' }
 
     # -- flash responder
     self.responder = Wobapphelpers::Responders
