7ebedb2d0493e17aec0d93844c7a5b231c1f0423
Wolfgang Barth <wob@swobspace.net>
2025-01-05 14:00:40 +0100

    update datatables controller

--- spec/dummy/app/javascript/controllers/datatables_controller.js
+++ spec/dummy/app/javascript/controllers/datatables_controller.js
@@ -9,49 +9,71 @@ export default class extends Controller {
   }
 
   initialize() {
-    var myTable = $.fn.dataTable;
-    $.extend( true, myTable.Buttons.defaults, {
-      "dom": { "button": { "className": 'btn btn-outline-secondary btn-sm' } }
-    })
   }
 
   connect() {
+    // overcome morph problems
+    this.element.setAttribute("data-action",
+                              "turbo:morph-element->datatables#reconnect"
+                             )
+    let _this = this
     let dtOptions = {}
     this.compileOptions(dtOptions)
-    if (!this.simpleValue) {
-      this.setInputFields()
-    }
-    const table = $(this.element.querySelector('table'))
 
-    // prepare options, optional add remote processing (not yet implemented)
+    const table = $(this.element.querySelector('table'))
+    console.log(table[0].id)
+    // prepare options
     let dtable = $(table).DataTable(dtOptions)
 
-    // process search input
-    dtable.columns().eq(0).each((colIdx) => {
-      $('input[name=idx'+colIdx+']').on( 'keyup change', function() {
-	dtable.column(colIdx).search(this.value).draw()
-      })
-    })
+    // catch column visibility change
+    this.colvis_change_listener(dtable)
+
+    if (!this.simpleValue) {
+      // wait for init complete if server side processing
+      // else call setInputFields directly
+      if (dtable.page.info().serverSide == true) {
+        dtable.on('init.dt', function() {
+          _this.setInputFields(dtable)
+          _this.process_search_input(dtable)
+        })
+      } else {
+        this.setInputFields(dtable)
+        this.process_search_input(dtable)
+      }
+    }
+
   } // connect
 
+  disconnect() {
+  }
+
   // search fields for each column
-  setInputFields() {
+  setInputFields(dtable) {
+    // console.log(dtable.tables(0).columns)
     this.element.querySelectorAll("table tfoot th:not([class='nosearch'])")
         .forEach((th, idx) => {
-          th.insertAdjacentHTML('afterbegin', this.searchField(idx))
+          let col = th.getAttribute("data-dt-column")
+          let text = ''
+          let state = dtable.state();
+          if (state) {
+            text = state.columns[col].search.search
+          }
+          th.insertAdjacentHTML('afterbegin', this.searchField(col, text))
         })
   }
 
   // single search input field
-  searchField(idx) {
-    return `<input type="text" placeholder="search" name="idx${idx}" />`
+  searchField(idx, text) {
+    return `<input type="text" placeholder="search" name="idx${idx}" value="${text}"/>`
   }
 
   // datatables options
   compileOptions(options) {
     // common options
     options.pagingType = "full_numbers"
-    options.stateSave = false
+    options.responsive = true
+    options.stateSave = true
+    options.stateDuration = 60 * 60 * 24
     options.lengthMenu = [ [10, 25, 100, 250, 1000], [10, 25, 100, 250, 1000] ]
     options.columnDefs = [ { "targets": "nosort", "orderable": false },
                            { "targets": "notvisible", "visible": false },
@@ -70,32 +92,93 @@ export default class extends Controller {
   }
 
   simpleOptions(options) {
-    options.dom =  "<'row'<'col-sm-12'tr>>" +
-                   "<'row'<'col pt-2'l><'col'i><'col'p>>"
+    options.dom =  "<'row '<'col-sm-12'tr>>" +
+                   "<'row mt-2 justify-content-between'<'col-md-auto me-auto mt-1'l><'col-md-auto me-auto mt-2'i><'col-md-auto ms-auto'p>>"
     options.pagingType = "numbers"
   }
 
 
   buttonOptions(options) {
-    options.dom = "<'row'<'col'l><'col d-none d-lg-block'B><'col'f>>" +
-                    "<'row'<'col-sm-12'tr>>" +
-                    "<'row'<'col'i><'col'p>>"
-    options.buttons = [
-                        { "extend": 'excel',
-	                  "exportOptions": { "search": ':applied' } },
-                        { "extend": 'pdf',
-	                  "orientation": 'landscape',
-	                  "pageSize": 'A4',
-	                  "exportOptions": { "columns": ':visible',
-	                                     "search": ':applied' } },
-                        { "extend": 'print'},
-                        { "extend": 'colvis', "columns": ':gt(0)' } ]
+    options.dom = "<'row mt-2 justify-content-between'<'col-md-auto me-auto'l><'col-md-auto'B>>" +
+                    "<'row mt-2 justify-content-md-center'<'col-sm-12'tr>>" +
+                    "<'row mt-2 justify-content-between'<'col-md-auto me-auto'i><'col-md-auto ms-auto'p>>"
+    options.buttons = {
+      dom: {
+        button: {
+          tag: 'button',
+          className: 'btn btn-outline-secondary btn-sm'
+        }
+      },
+      buttons: [
+                 { "text": 'Reset',
+                    "action": function(e, dt, node, config) {
+                                dt.state.clear();
+                                window.location.reload();
+                              }},
+                 { "extend": 'csv',
+	           "exportOptions": { "columns": ':visible',
+                                      "search": ':applied' } },
+                 { "extend": 'excel',
+	           "exportOptions": { "columns": ':visible',
+                                      "search": ':applied' } },
+                 { "extend": 'pdf',
+	           "orientation": 'landscape',
+	           "pageSize": 'A4',
+	           "exportOptions": { "columns": ':visible',
+	                              "search": ':applied' } },
+                 { "extend": 'print'},
+                 { "extend": 'colvis', "columns": ':gt(0)' }
+               ]
+    }
   }
 
   remoteOptions(options) {
+    //
+    let csrf = document.head.querySelector('meta[name="csrf-token"]')
+    let token = "not available"
+    if (csrf != null) {
+      token = csrf.getAttribute('content')
+    }
     options.searchDelay = 400
     options.processing = true
     options.serverSide = true
-    options.ajax = { "url": this.urlValue, "type": "POST" }
+    options.ajax = {
+      'url': this.urlValue,
+      'type': 'POST',
+      'beforeSend': function(request) {
+        request.setRequestHeader("X-CSRF-Token", token)
+      }
+    }
+  }
+
+  // fix morph problems
+  reconnect() {
+    this.disconnect()
+    this.connect()
+  }
+
+  colvis_change_listener(dtable) {
+    let _this = this
+    dtable.on('column-visibility.dt', function (e, settings, column, state) {
+      if (state) {
+        let th = e.target.querySelector('tfoot th[data-dt-column="' + column + '"]')
+        let sf = th.querySelector('input')
+        if (!sf) {
+          th.insertAdjacentHTML('afterbegin', _this.searchField(column, ''))
+        }
+        $('input[name=idx'+column+']').on( 'keyup change', function() {
+          dtable.column(column).search(this.value).draw()
+        })
+      }
+    })
+  }
+
+  process_search_input(dtable) {
+    // process search input
+    dtable.columns().eq(0).each((colIdx) => {
+      $('input[name=idx'+colIdx+']').on( 'keyup change', function() {
+	dtable.column(colIdx).search(this.value).draw()
+      })
+    })
   }
 } // Controller
--- spec/dummy/app/javascript/src/datatables-bs5.js
+++ spec/dummy/app/javascript/src/datatables-bs5.js
@@ -7,20 +7,12 @@ window.JSZip = JSZip
 // used for PDF button
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+pdfMake.addVirtualFileSystem(pdfFonts);
 
+import DataTable from 'datatables.net-bs5';
+import 'datatables.net-buttons-bs5';
+import 'datatables.net-responsive-bs5';
+import 'datatables.net-buttons/js/buttons.colVis.mjs';
+import 'datatables.net-buttons/js/buttons.html5.mjs';
+import 'datatables.net-buttons/js/buttons.print.mjs';
 
-import DataTable from 'datatables.net-bs5'
-window.DataTable = DataTable()
-
-import buttons from 'datatables.net-buttons-bs5'
-buttons(window, $)
-
-import columnVisibility from 'datatables.net-buttons/js/buttons.colVis.js'
-columnVisibility(window, $)
-
-import buttonsHtml5 from 'datatables.net-buttons/js/buttons.html5.js'
-buttonsHtml5(window, $)
-
-import buttonsPrint from 'datatables.net-buttons/js/buttons.print.js'
-buttonsPrint(window, $)
